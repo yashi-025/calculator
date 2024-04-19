9c1cf7160b20db5a92be3d94f446104050aed45c
yashi-025 <146928623+yashi-025@users.noreply.github.com>
2024-04-20 01:33:27 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print("enter number:")
+a,b=map(int,input().split())
+print("enter operation to be performed:")
+operator=input()
+print("output:")
+if operator=='+':
+    print(a+b)
+elif operator=='-':
+    print(a-b)
+elif operator=='*':
+    print(a*b)
+elif operator=='/':
+    print(a/b)    
